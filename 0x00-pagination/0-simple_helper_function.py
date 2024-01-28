8a0582da00712deff275480e0b314d6512605d1d
root <root@DESKTOP-TKLJENL.localdomain>
2024-01-29 00:06:19 +0300

    simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+def index_range(page, page_size):
+    """
+    return a tuple of size two containing a start index and an end
+    index corresponding to the range of
+    indexes to return in a list for those particular pagination parameters.
+    """
+    if page <= 0 or page_size <= 0:
+        raise ValueError("Must be positive numbers")
+    first_index = (page - 1) * page_size
+    last_index = first_index + page_size - 1
+
+    return first_index, last_index
