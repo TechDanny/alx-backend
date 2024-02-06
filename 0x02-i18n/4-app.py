18f8be3077376f81f9bb226dd7d7386afee36285
root <kokidanny15@gmail.com>
2024-02-07 01:24:54 +0300

    flask app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -30,10 +30,14 @@ def get_locale():
     """
     determines the best match with our supported languages.
     """
-    locale = request.args.get("locale")
-
-    if locale and locale in app.config["LANGUAGES"]:
-        return locale
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
