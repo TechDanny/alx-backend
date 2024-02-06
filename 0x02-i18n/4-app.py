3713edd14393f477ad229d74b77612748afa637e
root <kokidanny15@gmail.com>
2024-02-07 01:16:13 +0300

    flask app

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config():
+    """
+    set up babel configurations
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+babel.init_app(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    determines the best match with our supported languages.
+    """
+    locale = request.args.get("locale")
+
+    if locale and locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index():
+    """
+    define route index
+    """
+    return render_template("4-index.html")
