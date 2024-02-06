cd3a7c9daba0f2af0cde5af27474f57a9b99f99d
root <kokidanny15@gmail.com>
2024-02-06 23:42:52 +0300

    app file

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Basic Babel setup
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """
+    set up babel configurations
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+babel.init_app(app)
