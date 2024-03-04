d00fac97cd60917bcbff71c43c236999f74f0681
root <kokidanny15@gmail.com>
2024-03-05 02:49:52 +0300

    Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, value) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(value);
+        }
+    });
+}
+
+async function main() {
+    await displaySchoolValue('Holberton');
+    setNewSchool('HolbertonSanFrancisco', '100');
+    await displaySchoolValue('HolbertonSanFrancisco');
+  }
+  
+  main();
