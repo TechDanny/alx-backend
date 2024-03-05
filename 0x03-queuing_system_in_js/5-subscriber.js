357f80b971aaebf8d00620d2aa91030dca3888a1
root <kokidanny15@gmail.com>
2024-03-05 23:55:02 +0300

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+const redis = require('redis');
+
+const subscriber = redis.createClient();
+
+subscriber.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+
+subscriber.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+subscriber.subscribe('holberton school channel');
+
+subscriber.on('message', (channel, message) => {
+  console.log(`Message received on channel ${channel}: ${message}`);
+
+  if (message === 'KILL_SERVER') {
+    subscriber.unsubscribe();
+    subscriber.quit();
+  }
+});
+
